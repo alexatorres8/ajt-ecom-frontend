3c08c0f529ff9b2c5557e389d1c0c2a1cfe4137b
alexatorres8 <ajt8@live.com>
2021-10-25 18:59:36 -0400

    setup project

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Jasmine's Art {`&`} Graphics</h1>
+        <h2>Beep!!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
