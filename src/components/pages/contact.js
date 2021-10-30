d9a79785b780b8ee760dea1658ff755576ee34de
alexatorres8 <ajt8@live.com>
2021-10-29 23:10:43 -0400

    added social media buttons to contact page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { Component } from "react";
 
+import SocialMediaButtons from 'react-social-media-buttons';
 import contactPicture from "../../../static/assets/images/jasmine02.jpg";
 
-export default function() {
-    return (
-        <div className="page-layout-wrapper">
-            <div
-                className="left-column"
-                style={{
-                    background: "url(" + contactPicture + ") no-repeat",
-                    backgroundSize: "cover",
-                    backgroundPosition: "right"
-                }}
-            />
+class Contact extends Component {
+ 
+    render() {
+        return (
+            <div className="page-layout-wrapper">
+                <div
+                    className="left-column"
+                    style={{
+                        background: "url(" + contactPicture + ") no-repeat",
+                        backgroundSize: "cover",
+                        backgroundPosition: "right"
+                    }}
+                />
 
-            <div className="right-column">
-                <div className="title-header">Contact Me</div>
+                <div className="right-column">
+                    <div className="title-header">Contact Me</div>
 
-                <div className="page-text">
-                    <p><b>Email:</b> jazctor(a)gmail.com</p>
-                    <p><b>Instagram:</b> @foundpeace</p>
+                    <div className="page-text">
+                        <p><b>Email:</b> jazctor(a)gmail.com</p>
+
+                        <SocialMediaButtons
+                            links={
+                                [
+                                    "https://www.linkedin.com/in/jasmine-torres-892041222/",
+                                    "https://www.instagram.com/foundpeace/"
+                                ]
+                            }
+
+                            buttonStyle={{
+                                width: "63px",
+                                height: "63px",
+                                margin: "0px 12px",
+                                backgroundColor: "transparent"
+                            }}
+
+                            iconStyle={{ color: "#3e4749" }}
+                            openNewTab={true}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+export default Contact;
